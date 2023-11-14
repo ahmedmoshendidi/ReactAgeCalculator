ee9178c296749cffc183ba9117c3fb0bb536e544
ahmedmoshendidi <ahmedmoalshendidi@gmail.com>
2023-11-14 18:21:50 +0200

    change css files name

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Form from "./components/Form";
-import "./App.CSS";
+import "./AppStyle.CSS";
 export default function App() {
   return (
     <div className="container">
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "./Form.css";
+import "./FormStyle.css";
 
 export default function Form() {
   const [dateOnScreen, setDateOnScreen] = useState({
